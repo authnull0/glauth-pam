d5a13d1f8cfe2096aadf53f87d2855390c4a6386
Hussain <pshussain@gmail.com>
2023-01-30 11:05:54 +0530

    otp: Added cron code to pull users

--- v2/pkg/server/sync.go
+++ v2/pkg/server/sync.go
@@ -1,11 +1,21 @@
 package server
 
-func synchronize() {
 
+import (
+	"gopkg.in/robfig/cron.v2"
+)
+func synchronize() {
+	
 }
 
 func pullUsers() {}
 
 func addToConfig() {}
 
-func run() {}
+func run() {
+
+	c:= cron.New
+
+	c.AddFunc("@every 10m", synchronize())
+	c.Start()
+}
