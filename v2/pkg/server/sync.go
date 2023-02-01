5dd1c047cc3645d6aad5f40a1e4ebd7398ee63ac
Hussain <pshussain@gmail.com>
2023-02-02 01:44:24 +0530

    otp: Added correct API request struct

--- v2/pkg/server/sync.go
+++ v2/pkg/server/sync.go
@@ -12,20 +12,34 @@ import (
 	authnull "github.com/glauth/glauth/v2/pkg/external"
 )
 
+type UserRequest struct {
+	DomainId 	int    	`json:"domainId"`
+	PageId   	int  	`json:"pageId"`
+	PageSize 	int		`json:"pageSize"`
+	Filter   	string 	`json:"filter"`
+	InstanceIds	[]int  	`json:"instanceIds"`
+}
+
+
 var authnull0 authnull.Authnull
 
 type Sync struct{} 
 func (s *Sync) synchronize() {
 	fmt.Println(" Hai Cron")
+	s.pullUsers()
 }
 
 func (s *Sync) pullUsers() {
 
-	postBody, _ := json.Marshal(map[string]string{
-      "api-key":  "abc",
+	postBody, _ := json.Marshal(&UserRequest{ 
+		DomainId: 1,
+		PageId: 1,
+		PageSize: 10,
+		Filter: "ssh",
+		InstanceIds: []int{1,2},
     })
 	responseBody := bytes.NewBuffer(postBody)
-    resp, err := http.Post("https://client.did.kloudlearn.com/api/v1/lums/listAllEpmUsers", "application/json", responseBody)
+    resp, err := http.Post("https://api.authnull.kloudlearn.com/api/v1/lums/listAllEpmUsers", "application/json", responseBody)
 	if err != nil {
       log.Fatalf("An Error Occured %v", err)
    }
@@ -47,8 +61,8 @@ func (s Sync) Run() {
 
 	c:= cron.New()
 	
-	var sync Sync
+	var synchronizer Sync
 	authnull0.Init()
-	c.AddFunc("@every 1s", func(){ sync.synchronize()})
+	c.AddFunc("@every 10s", func(){ synchronizer.synchronize()})
 	c.Start()
 }
