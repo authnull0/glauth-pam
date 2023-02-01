6b44fbaa5a6e4975c5358e0dad20931a2f499afd
Hussain <pshussain@gmail.com>
2023-02-02 01:00:54 +0530

    otp: Added sync API

--- v2/pkg/server/sync.go
+++ v2/pkg/server/sync.go
@@ -2,32 +2,53 @@ package server
 
 
 import (
+	"bytes"
 	"gopkg.in/robfig/cron.v2"
 	"fmt"
+   "net/http"
+   "encoding/json"
+	"log"
+	"io/ioutil"
 	authnull "github.com/glauth/glauth/v2/pkg/external"
 )
 
 var authnull0 authnull.Authnull
 
 type Sync struct{} 
-func synchronize() {
+func (s *Sync) synchronize() {
 	fmt.Println(" Hai Cron")
 }
 
-func pullUsers() {
+func (s *Sync) pullUsers() {
+
+	postBody, _ := json.Marshal(map[string]string{
+      "api-key":  "abc",
+    })
+	responseBody := bytes.NewBuffer(postBody)
+    resp, err := http.Post("https://client.did.kloudlearn.com/api/v1/lums/listAllEpmUsers", "application/json", responseBody)
+	if err != nil {
+      log.Fatalf("An Error Occured %v", err)
+   }
+   defer resp.Body.Close()
+//Read the response body
+   body, err := ioutil.ReadAll(resp.Body)
+   if err != nil {
+      log.Fatalln(err)
+   }
+   fmt.Println(string(body))
 
 }
 
-func addToConfig() {
+func (s *Sync) addToConfig() {
 
 }
 
-func (s Sync)Run() {
+func (s Sync) Run() {
 
 	c:= cron.New()
 	
-	
+	var sync Sync
 	authnull0.Init()
-	c.AddFunc("@every 1s", func(){ synchronize()})
+	c.AddFunc("@every 1s", func(){ sync.synchronize()})
 	c.Start()
 }
